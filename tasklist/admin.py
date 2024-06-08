8a88ab7a2a83560f6061e6415f80759824bb0428
AndreiChpakovski <achpakovski@gmail.com>
2024-06-07 20:55:51 -0400

    Update admin.py

--- tasklist/admin.py
+++ tasklist/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Manager
+from .models import Manager, Task, Comment
 
 # Register your models here.
 
 admin.site.register(Manager)
+admin.site.register(Task)
+admin.site.register(Comment)
