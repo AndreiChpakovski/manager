3ab394abe8a1b3f28cd955f8e667d88d63d9ca86
AndreiChpakovski <achpakovski@gmail.com>
2024-06-07 20:55:00 -0400

    Update urls.py

--- tasklist/urls.py
+++ tasklist/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
 from . import views
+from .views import task_list, task_detail, task_create, task_update, task_delete
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.add, name='add'),
     path('update/<int:task_id>/', views.update, name='update'),
-    path('delete/<int:task_id>/', views.delete, name='delete')
+    path('delete/<int:task_id>/', views.delete, name='delete'),
+    path('', views.task_list, name='task_list'),
+    path('tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('tasks/new/', views.task_create, name='task_create'),
+    path('tasks/<int:task_id>/edit/', views.task_update, name='task_update'),
+    path('tasks/<int:task_id>/delete/', views.task_delete, name='task_delete'),
 ]
