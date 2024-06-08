2e4d5b05f484178a0a46560e13fd3aee595fc553
AndreiChpakovski <achpakovski@gmail.com>
2024-06-07 20:56:59 -0400

    Update models.py

--- tasklist/models.py
+++ tasklist/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+
 # Create your models here.
 class Manager(models.Model):
     title = models.CharField('Name of the homework', max_length=500)
@@ -10,6 +11,23 @@ class Manager(models.Model):
         verbose_name = 'Task'
         verbose_name_plural = 'Tasks'
 
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    is_complete = models.BooleanField(default=False)
+
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment on {self.task.title} by {self.created_at}"
+
+
+def __str__(self):
+    return self.title
 
